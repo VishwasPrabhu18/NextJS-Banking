98af7bfdcb3349917cfb50458138ae029b282253
Vishwas Prabhu <vishwasprabhu@qliclabs.com>
2024-05-14 20:32:56 +0530

    Connect Multiple Bank Accounts is completed with UI and functionality

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -7,6 +7,7 @@ import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 import Footer from './Footer';
+import PlaidLink from '@/components/PlaidLink';
 
 const Sidebar = ({ user }: SiderbarProps) => {
 
@@ -49,7 +50,7 @@ const Sidebar = ({ user }: SiderbarProps) => {
           )
         })}
 
-        USER
+        <PlaidLink user={user} />
       </nav>
 
       <Footer user={user} type="desktop" />
